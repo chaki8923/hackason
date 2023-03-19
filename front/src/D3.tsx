d9886dc24e4f2b283cb1d1b569d685818b369415
chaki <aowp.contact@gmail.com>
2023-03-19 17:21:54 +0900

    地図描画まで

--- front/src/D3.tsx
+++ front/src/D3.tsx
@@ -1,65 +1,115 @@
+//
+// 以下のページで詳細を解説しています
+// https://qiita.com/alclimb/items/31d4360c74a8f8935256
+//
 
-import React from 'react';
 import * as d3 from 'd3';
-
+import React, { useEffect } from 'react';
 // GeoJsonファイルを読み込み
-import geoJson from "~/assets/japan.geo.json";
+import geoJson from './japan.geo.json';
+import sampleJson from './todouhuken.json';
+import { FeatureCollection, Feature } from 'geojson';
+interface PrefectureData {
+  name: string;
+  value: number;
+}
 
-async function main() {
+function D3() {
   const width = 400; // 描画サイズ: 幅
   const height = 400; // 描画サイズ: 高さ
-  const centerPos = [137.0, 38.2]; // 地図のセンター位置
+  const centerPos: [number, number] = [137.0, 38.2]; // 地図のセンター位置
   const scale = 1000; // 地図のスケール
 
-  // 地図の投影設定
-  const projection = d3
-    .geoMercator()
-    .center(centerPos)
-    .translate([width / 2, height / 2])
-    .scale(scale);
+  useEffect(() => {
+    // 地図の投影設定
+    const projection = d3
+      .geoMercator()
+      .center(centerPos)
+      .translate([width / 2, height / 2])
+      .scale(scale);
 
-  // 地図をpathに投影(変換)
-  const path = d3.geoPath().projection(projection);
+    // 地図をpathに投影(変換)
+    const path: any = d3.geoPath().projection(projection);
 
-  // SVG要素を追加
-  const svg = d3
+    // 都道府県ごとのデータを取得するAPIのエンドポイントとする
+    // console.log(sampleJson.prefectures);
+    interface MyData {
+      name: string;
+      capital: string;
+      population: number;
+      area: number;
+      cities: {
+          name: string;
+          population: number;
+      }[];
+  }[]
+    
+    
+    const apiEndpoint = 'https://my.api.com/data';
+    
+    const fetchData = async () => {
+      const [apiResponse, geoJsonResponse] = await Promise.all([sampleJson.prefectures, geoJson]) as [MyData[], FeatureCollection];
+      // const [apiResponse, geoJsonResponse] = await Promise.all([d3.json(apiEndpoint), geoJson]) as [MyData[], FeatureCollection];
+    geoJsonResponse.features.map((feature: Feature) => {
+        const value = apiResponse.find((d) => d.name === feature.properties!.name_ja)?.name || 0;
+        return {
+          ...feature,
+          properties: {
+            ...feature.properties,
+            value,
+          },
+        };
+      });
+      
+      const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([0, d3.max(apiResponse, (d) => d.population)!]);
+      console.log("ドヤ！");
+      // console.log(colorScale(d.properties.value));
+      
+      // do something with featuresWithValues and colorScale
+    };
+    
+    
+    // fetchData();
+    
+    
+    // SVG要素を追加
+    const svg = d3
     .select(`#map-container`)
     .append(`svg`)
-    .attr(`viewBox`, `0 0 ${width} ${height}`)
+    .attr(`viewBox`, `0 -15 ${width} ${height}`)
     .attr(`width`, `100%`)
     .attr(`height`, `100%`);
+    
+    //
+    // [ メモ ]
+    // 動的にGeoJsonファイルを読み込む場合は以下のコードを使用
+    // const geoJson = await d3.json(`/japan.geo.json`);
+    //
 
-  // 
-  // [ メモ ]
-  // 動的にGeoJsonファイルを読み込む場合は以下のコードを使用
-  // const geoJson = await d3.json(`/japan.geo.json`);
-  // 
-
-  // 都道府県の領域データをpathで描画
-  svg
-    .selectAll(`path`)
-    .data(geoJson.features)
-    .enter()
-    .append(`path`)
-    .attr(`d`, path)
-    .attr(`stroke`, `#666`)
-    .attr(`stroke-width`, 0.25)
-    .attr(`fill`, `#2566CC`)
-    .attr(`fill-opacity`, (item: any) => {
-      // メモ
-      // item.properties.name_ja に都道府県名が入っている
-
-      // 透明度をランダムに指定する (0.0 - 1.0)
-      return Math.random();
-    })
-
-    /**
-     * 都道府県領域の MouseOver イベントハンドラ
-     */
-    .on(`mouseover`, function(item: any) {
-      // ラベル用のグループ
+    // 都道府県の領域データをpathで描画
+    svg
+      .selectAll(`path`)
+      .data(geoJson.features)
+      .enter()
+      .append(`path`)
+      .attr(`d`, path)
+      .attr(`stroke`, `#666`)
+      .attr(`stroke-width`, 0.25)
+      .attr(`fill`, `#333`)
+      .attr(`fill-opacity`, (item: any) => {
+        // メモ
+        // item.properties.name_ja に都道府県名が入っている
+        // 透明度をランダムに指定する (0.0 - 1.0)
+        return Math.random();
+      })
+      /**
+       * 都道府県領域の MouseOver イベントハンドラ
+       */
+      .on(`mouseover`, function (item: any) {
+         // ラベル用のグループ
       const group = svg.append(`g`).attr(`id`, `label-group`);
-
+      // console.log('アイテム2');
+      // console.log(item.properties.name_ja);
       // 地図データから都道府県名を取得する
       const label = item.properties.name_ja;
 
@@ -72,14 +122,15 @@ async function main() {
         .attr(`fill`, `#fff`);
 
       // テキストを追加
+      // console.log(label);
       const textElement = group
         .append(`text`)
         .attr(`id`, `label-text`)
         .text(label);
 
       // テキストのサイズから矩形のサイズを調整
-      const padding = { x: 5, y: 0 };
-      const textSize = textElement.node().getBBox();
+      const padding = { x: 5, y: 20 };
+      const textSize = textElement.node()!.getBBox();
       rectElement
         .attr(`x`, textSize.x - padding.x)
         .attr(`y`, textSize.y - padding.y)
@@ -89,52 +140,26 @@ async function main() {
       // マウス位置の都道府県領域を赤色に変更
       d3.select(this).attr(`fill`, `#CC4C39`);
       d3.select(this).attr(`stroke-width`, `1`);
-    })
+      })
+  
+      /**
+       * 都道府県領域の MouseOut イベントハンドラ
+       */
+      .on(`mouseout`, function (item: any) {
+        // ラベルグループを削除
+        svg.select('#label-group').remove();
+  
+        // マウス位置の都道府県領域を青色に戻す
+        d3.select(this).attr(`fill`, `#2566CC`);
+        d3.select(this).attr(`stroke-width`, `0.25`);
+      });
+  })
+  
+    return (
+      <div id="map-container">
 
-    /**
-     * 都道府県領域の MouseMove イベントハンドラ
-     */
-    .on("mousemove", function(item: any) {
-      // テキストのサイズ情報を取得
-      const textSize = svg
-        .select("#label-text")
-        .node()
-        .getBBox();
-
-      // マウス位置からラベルの位置を指定
-      const labelPos = {
-        x: d3.event.offsetX - textSize.width,
-        y: d3.event.offsetY - textSize.height
-      };
-
-      // ラベルの位置を移動
-      svg
-        .select("#label-group")
-        .attr(`transform`, `translate(${labelPos.x}, ${labelPos.y})`);
-    })
-
-    /**
-     * 都道府県領域の MouseOut イベントハンドラ
-     */
-    .on(`mouseout`, function(item: any) {
-      // ラベルグループを削除
-      svg.select("#label-group").remove();
-
-      // マウス位置の都道府県領域を青色に戻す
-      d3.select(this).attr(`fill`, `#2566CC`);
-      d3.select(this).attr(`stroke-width`, `0.25`);
-    });
+      </div>
+    );
 }
-
-main();
-
-function D3(){
-  return(
-    <div>
-
-    </div>
-  )
-}
-
-
 export default D3;
+
